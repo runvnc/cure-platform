71b13f06502e00a836c29c2e2786dcf707b00766
root <root@so.site-orders.com>
2011-10-22 23:15:47 -0400

    use git for node

--- node.sh
+++ node.sh
@@ -33,18 +33,25 @@ if [ $platform = 'linux' ]; then
   echo 'Update completed'
   # Install help app
   apt-get -y install libssl-dev git-core pkg-config build-essential curl gcc g++
-  # Download & Unpack Node.js - v. 0.4.12
-  echo 'Download Node.js - v. 0.4.12'
-  mkdir /tmp/node-install
-  cd /tmp/node-install
-  wget http://nodejs.org/dist/node-v0.4.12.tar.gz
-  tar -zxf node-v0.4.12.tar.gz
-  echo 'Node.js download & unpack completed'
-  # Install Node.js
+  # Download & Unpack Node.js - v 0.5.9
+
+
+  echo 'Download Node.js - v 0.5.9'
+  mkdir /opt/node-install
+  cd /opt/node-install
+  git clone --depth 1 git://github.com/joyent/node.git
+  cd node
+  git checkout v0.5.9
+  #wget http://nodejs.org/dist/node-v0.4.12.tar.gz
+  #tar -zxf node-v0.4.12.tar.gz
+  echo 'Node.js clone complete'
+  ## Install Node.js
   echo 'Install Node.js'
-  cd node-v0.4.12
-  ./configure && make && make install
+  cd node
+  ./configure && make -j2 && make install
   echo 'Node.js install completed'
+
+
   # Install Node Package Manager
   echo 'Install Node Package Manager'
   curl http://npmjs.org/install.sh | sudo sh
